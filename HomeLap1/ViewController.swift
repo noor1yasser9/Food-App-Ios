3e8a1d08bfd06b483ee46a0e9814619282b08802
Nurbk229 <mrh61037@gmail.com>
2021-04-03 03:24:27 -0700

    add home view controller

--- HomeLap1/ViewController.swift
+++ HomeLap1/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let storybord = UIStoryboard(name: "Main", bundle: .main)
+    
 
+    
+    @IBOutlet weak var mTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mTable.register(UINib(nibName: "ResTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
+        mTable.delegate = self
+        mTable.dataSource = self
+        
     }
 
+    
+    
 
+}
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resturantList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ResTableViewCell
+        
+        cell.bindData(rest: resturantList[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        	
+        let cv = storybord.instantiateViewController(identifier: "DetailsViewController") as! DetailsViewController
+        cv.title = "Details Resturant"
+        cv.resturant = resturantList[indexPath.row]
+        self.navigationController?.pushViewController(cv, animated: true)
+        
+    }
 }
 
